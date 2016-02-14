f0babcb1c5ddc047f862a98c0bb620dd7d36a69e
Pippin <piyawat@ddproperty.com>
2016-02-15 03:54:07 +0700

    no message

---
+++ ddjobfair/data.js
@@ -0,0 +1,14 @@
+var currentQuestion = {};
+
+var question = [
+  { q: "Q1", o: "O", x1: "X", x2: "X", x3: "X" },
+  { q: "Q2", o: "O", x1: "X", x2: "X", x3: "X" },
+  { q: "Q3", o: "O", x1: "X", x2: "X", x3: "X" },
+  { q: "Q4", o: "O", x1: "X", x2: "X", x3: "X" },
+  { q: "Q5", o: "O", x1: "X", x2: "X", x3: "X" },
+  { q: "Q6", o: "O", x1: "X", x2: "X", x3: "X" },
+  { q: "Q7", o: "O", x1: "X", x2: "X", x3: "X" },
+  { q: "Q8", o: "O", x1: "X", x2: "X", x3: "X" },
+  { q: "Q9", o: "O", x1: "X", x2: "X", x3: "X" },
+  { q: "Q10", o: "O", x1: "X", x2: "X", x3: "X" }
+];
--- ddjobfair/script.js
+++ ddjobfair/script.js
@@ -1,5 +1,16 @@
-$(document).on("pagecreate","#pageone",function(){
-  $("p").on("tap",function(){
-    $(this).hide();
-  });
+$(function(){
+  renderQuestion();
 });
+
+function renderQuestion () {
+  var content = getTemplate("question");
+  renderBody(content);
+}
+
+function renderBody (content) {
+  $('#game-body').html(content);
+}
+
+function getTemplate (templateId) {
+  return $("#template #" + templateId);
+}
