c14c156c059c4d1233c8259584e5f8869af7e3bf
Pippin <piyawat@ddproperty.com>
2016-02-15 10:20:52 +0700

    no message

--- xmas/assets/js/app.js
+++
@@ -1,6 +0,0 @@
-$(function() {
-	// force redirect to #/
-	if (window.location.href.indexOf('#') == -1) {
-		window.location.href = _.trim(window.location.href, '/') + '/#/';
-	}
-});
--- xmas/assets/js/data/operation.js
+++
@@ -1,7 +0,0 @@
-Staff.operation = [
-	"1. Pailin Kitbumrung",
-	"2. Sarinthip Trakuljae",
-	"3. Sunee Rirkvaleekul",
-	"4. พี่นา (แม่บ้าน)",
-	"5. พี่สุทิน (Messenger)"
-];
