7e277e7038737c27e4696c8c6023fdf41f778a92
Pippin Chan <pippinchan@Pippin.local>
2016-05-10 07:24:54 +0700

    no message

--- ddluckydraw/game.js
+++ ddluckydraw/game.js
@@ -1,8 +1,19 @@
+var numKeys = 5; // number of key images
+var displayKeys = 10; // number of keys to show
 var stock = {};
 var playArea;
 
+// https://www.kirupa.com/html5/random_numbers_js.htm
+function random(Low, High) {
+  return Math.floor(Math.random()*(1+High-Low))+Low;
+}
+
 function elHouse() {
   return $("<div>").addClass("item-house").droppable();
+}
+
+function elKey(number) {
+  return $("<div>").addClass("item-key" + (number % numKeys)).draggable();
 }
 
 function renderPlayArea() {
@@ -10,6 +21,12 @@ function renderPlayArea() {
 
   var house = elHouse().appendTo(playArea);
   house.css({ left: "40%" });
+
+  var key;
+  for (var k = 0; k < displayKeys; k++) {
+    key = elKey(k);
+    key.appendTo(playArea).css({ position: "absolute", left: random(0,70) + "%", top: random(20,70) + "%" });
+  }
 }
 
 $(function() {
